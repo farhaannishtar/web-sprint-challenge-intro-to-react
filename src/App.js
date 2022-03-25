04ecb69158b2687f590d3a651442a8847c1705a4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-03-25 14:41:32 -0400

    Rendered Character names on DOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Character from './components/Character.js'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +10,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+
+  useEffect(() => {
+    console.log('hi');
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log(res);
+        setCharacters(res.data);
+      })
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(character => {
+        return <Character character={character} characterName={character.name}/>
+      })  
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+  const {character} = props;
+
+  return (
+    <div>
+      <h1>{character.name}</h1>
+    </div>
+  );
+}
+
+export default Character;
